cfb59e60a682a0a92a7361ff1bb84ec6a6c1c1dc
Manny <emaldonado1402@gmail.com>
2023-01-15 03:08:18 -0500

    render a view for the get request

--- controllers/games.js
+++ controllers/games.js
@@ -1,7 +1,18 @@
 import { Game } from "../models/game.js";
 
 function index(req , res) {
-  console.log('GAMES')
+  Game.find({})
+  .then(games => {
+    res.render("games/index", {
+      games,
+      title : "games"
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect("/")
+  })
+
 }
 
 export {
