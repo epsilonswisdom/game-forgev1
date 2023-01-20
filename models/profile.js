fa823d2619f1508300541ca492c9b0e24cc267f1
Manny <emaldonado1402@gmail.com>
2023-01-19 23:14:44 -0500

    define the route for comments

--- models/profile.js
+++ models/profile.js
@@ -2,10 +2,16 @@ import mongoose from 'mongoose'
 
 const Schema = mongoose.Schema
 
+const commentSchema = new Schema({
+  content:String,
+  commenter: {type: Schema.Types.ObjectId, ref: "Profile"}
+})
+
 const profileSchema = new Schema({
   name: String,
   avatar: String,
   games : ["GAME"],
+  comments: [commentSchema]
 }, {
   timestamps: true
 })
